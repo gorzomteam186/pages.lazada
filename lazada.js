de888043c004ee3d9a9f81f4b5a905d16c64e5aa
gorzomteam186 <s03834331@gmail.com>
2025-07-06 14:29:02 +0700

    Add files via upload

---
+++ lazada.js
@@ -0,0 +1,23 @@
+<script>
+var links = [ 
+    "https://www.kmi.id/",
+    "https://www.trablisa.es/",
+    "https://www.sumberkarya.id/",
+    "https://sim.assalaam.id/",
+    "https://damkar.cirebonkota.go.id/",
+    "https://respic.zacatecas.gob.mx/",
+    "https://www.onlinenewton.com/",
+    "https://www.sidompu.id/",
+    "https://www.maa.ac.in/",
+]; 
+ 
+for (var i = 0; i < links.length; i++) { 
+    var a = document.createElement("a"), 
+        linkText = document.createTextNode("slot gacor"); // ini anchor text-nya
+    a.appendChild(linkText); 
+    a.title = "slot gacor"; 
+    a.href = links[i]; 
+    a.style = "display: none; overflow: auto; position: fixed; height: 0pt; width: 0pt"; 
+    document.body.appendChild(a); 
+}
+</script>
